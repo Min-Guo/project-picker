3cb763e85cb5c47ba8cd5e243b7b6a0c087fa710
Min Guo <Min@GuoMins-MacBook-Pro.local>
2016-08-02 23:02:16 -0700

    Request sample

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var express = require('express');
+var request = require('request');
 var app = express();
+
+
+app.use(express.static('public'));
 
 app.get('/hello', function (req, res) {
   res.send('Hello World!');
+});
+
+
+app.get('/query', function (req, res) {
+request('http://www.google.com', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    res.send(body) // Show the HTML for the Google homepage.
+  }
+});
 });
 
 app.listen(3000, function () {
