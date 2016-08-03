1eb571d8208cfc7c1e87a41e783151914330e69b
Min Guo <Min@GuoMins-MacBook-Pro.local>
2016-08-03 00:34:25 -0700

    Node.js post request topN work

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var request = require('request');
 var app = express();
+var formData = {
+	queryType : "topN",
+	dataSource : "tweet",
+	granularity : "all",
+	dimension : "hashTag",
+	threshold : 5,
+	metric : "count",
+	aggregations : [{
+		type : "count",
+		name : "count"
+	}],
+	intervals: ["2016-07-31T17:00:00.000/2016-08-01T18:00:00.000"]
+};
 
 
 app.use(express.static('public'));
@@ -11,12 +24,24 @@ app.get('/hello', function (req, res) {
 
 
 app.get('/query', function (req, res) {
-request('http://www.google.com', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    res.send(body) // Show the HTML for the Google homepage.
-  }
+	request({
+  uri: "http://ec2-54-83-35-212.compute-1.amazonaws.com:8082/druid/v2/?pretty",
+  method: "POST",
+  json: true,
+    headers: {
+        "content-type": "application/json",
+    },
+    body: formData
+  // form: formData
+}, function(error, response, body) {
+	res.send(JSON.stringify(body));
+	console.log(error);
+  console.log(body);
 });
+
 });
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
